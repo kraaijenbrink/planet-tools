03ca3309a63c0ca0b1a998ec85f6ecbd527820f9
Philip Kraaijenbrink <p.d.a.kraaijenbrink@uu.nl>
2024-02-20 09:47:40 +0100

    Add estimate of order area and impact on quota

--- search-and-order.py
+++ search-and-order.py
@@ -5,25 +5,26 @@ import time
 import io
 from datetime import datetime
 from shapely.geometry import shape, GeometryCollection, Polygon
-from pyproj import Proj
+from shapely.ops import transform
+import pyproj
+import geopandas as gpd
 
 # settings
-perform_dryrun = True          # first perform a dryrun to check settings and return before ordering massive amounts!
-order_name = 'Meretschi'
-aoi_file = 'filters/meretschi_aoi_tight.json'
-start_date = '2016-01-01'
-end_date   = '2023-01-01'
-max_cloud  = 0.25
-udm_required = True
-clip_to_aoi = True
+perform_dryrun  = True          # first perform a dryrun to check settings and return before ordering massive amounts!
+order_name      = 'Fieldwork France 2024'
+aoi_file        = 'filters/fieldwork-france-aoi.geojson'
+start_date      = '2016-01-01'
+end_date        = '2024-01-01'
+max_cloud       = 0.05
+udm_required    = True
+clip_to_aoi     = True
 harmonize_to_S2 = True
-assets = ["ortho_analytic_8b_sr","ortho_analytic_4b_sr"]
-product_bundle = "analytic_8b_sr_udm2,analytic_sr_udm2"    # second is fallback
-
+assets          = ["ortho_analytic_8b_sr","ortho_analytic_4b_sr"]
+product_bundle  = "analytic_8b_sr_udm2,analytic_sr_udm2"    # second is fallback
 
 # setup client
 api_key = os.environ.get("PL_API_KEY")
-client = api.ClientV1(api_key)
+client  = api.ClientV1(api_key)
 
 # load area of interest
 with open(aoi_file) as f:
@@ -31,11 +32,10 @@ with open(aoi_file) as f:
 
 # setup search filters
 geometry_filter = api.filters.geom_filter(aoi, "geometry")
-date_filter = api.filters.date_range("acquired", gte = start_date + "T00:00:00.001Z", lte = end_date + 'T23:59:59.999Z')
-cloud_filter = api.filters.range_filter("cloud_cover", lte=max_cloud)
-sr_filter = api.filters.asset_filter(assets)
-udm_filter = api.filters.asset_filter(["ortho_udm2"])
-
+date_filter     = api.filters.date_range("acquired", gte = start_date + "T00:00:00.001Z", lte = end_date + 'T23:59:59.999Z')
+cloud_filter    = api.filters.range_filter("cloud_cover", lte=max_cloud)
+sr_filter       = api.filters.asset_filter(assets)
+udm_filter      = api.filters.asset_filter(["ortho_udm2"])
 if udm_required:
     comb_filter = api.filters.and_filter(geometry_filter, date_filter, cloud_filter, sr_filter, udm_filter)
 else:
@@ -43,8 +43,8 @@ else:
 
 # build request and get paginated search results
 item_types = ['PSScene']
-request = api.filters.build_search_request(comb_filter, item_types)
-results = client.quick_search(request)
+request    = api.filters.build_search_request(comb_filter, item_types)
+results    = client.quick_search(request)
 
 # put all pages in one string
 output = io.StringIO()
@@ -52,10 +52,22 @@ results.json_encode(output, limit=int(1e5))
 results_all = json.loads(output.getvalue())
 no_of_features = len(results_all['features'])
 
+# get total area in km2 for total order
+polys_raw    = [Polygon(item['geometry']['coordinates'][0]) for item in results_all['features']]
+polys_df     = gpd.GeoDataFrame(index=range(len(polys_raw)), crs='epsg:4326', geometry=polys_raw )
+to_proj      = pyproj.Proj(proj='aea', lat_1=polys_df.total_bounds[1], lat_2=polys_df.total_bounds[2])
+polys_df_aea = polys_df.to_crs(str(to_proj))
+aoi_df_aea   = gpd.GeoDataFrame(index=[0], crs='epsg:4326', geometry=[Polygon(aoi['coordinates'][0])]).to_crs(str(to_proj))
+polys_in_aoi = polys_df_aea.clip(aoi_df_aea)
+if clip_to_aoi:
+    order_area   = sum(polys_in_aoi.area) * 1e-6
+else:
+    order_area   = sum(polys_df_aea.area) * 1e-6
+
 # get ids and split into chunks of less than 500 items for api to work
-ids = [item['id'] for item in results_all['features']]  # edit this so that you can split it for each 500 units
+ids       = [item['id'] for item in results_all['features']]
 order_lim = 400
-ids = [ids[i:i + order_lim] for i in range(0, len(ids), order_lim)]
+ids       = [ids[i:i + order_lim] for i in range(0, len(ids), order_lim)]
 
 # set toolchain json according to settings
 if clip_to_aoi & harmonize_to_S2:
@@ -67,7 +79,12 @@ elif harmonize_to_S2:
 else:
     toolchain = []
 
-print('Total items: ' + str(no_of_features))
+# show info
+print('\nTotal items: ' + str(no_of_features))
+print("Total order area: " + str(round(order_area,0)) + " km2 ("\
+      + str(round(order_area / 3e7 * 100, 2)) + "% of UU DPG quota)\n")
+
+# make separate orders for the chunks
 for i in range(len(ids)):
 
     part_no = "{:02d}".format(i+1)
