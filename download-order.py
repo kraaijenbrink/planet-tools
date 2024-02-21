240257dd1c724a7cae6494d84c0a5f47d3ffde25
Philip Kraaijenbrink <p.d.a.kraaijenbrink@uu.nl>
2024-02-21 06:14:42 +0100

    Minor tweaks

--- download-order.py
+++ download-order.py
@@ -1,6 +1,10 @@
 from planet import api
 import os
 import time
+import requests
+import time
+from multiprocessing import cpu_count
+from multiprocessing.pool import ThreadPool
 
 # setup client
 api_key = os.environ.get("PL_API_KEY")
@@ -8,11 +12,11 @@ client = api.ClientV1(api_key)
 
 # get order information
 orders = client.get_orders()
-order_ids = [x['id'] for x in orders.items_iter(1000) if 'PrinsHendrikZanddijk ' in x['name']]
-order_names = [x['name'] for x in orders.items_iter(1000) if 'PrinsHendrikZanddijk ' in x['name']]
+order_ids = [x['id'] for x in orders.items_iter(1000) if 'Fieldwork France ' in x['name']]
+order_names = [x['name'] for x in orders.items_iter(1000) if 'Fieldwork France ' in x['name']]
 
 # prep output location
-out_path = 'c:/workspace/planet-zanddijk/'
+out_path = 'c:/workspace/vwk1-imagery/planet-raw'
 if not os.path.exists(out_path):
     os.mkdir(out_path)
 
@@ -24,27 +28,27 @@ for i in range(len(order_ids)):
     if not os.path.exists(dirname):
         os.mkdir(dirname)
 
+    # prepare data
     order_data = client.get_individual_order(id)
-    # order_data_names = order_data.get()['name']
-    loc_urls = [item for item in order_data.items_iter(1000)]
+    order_results = order_data.get()['_links']['results']
+    filenames = [item['name'] for item in order_results]
+    basenames = [os.path.basename(fn) for fn in filenames]
+    download_urls = [item['location'] for item in order_results]
+    dest_urls = [os.path.join(dirname, fn) for fn in basenames]
+    inputs = zip(download_urls, dest_urls)
 
-    # download order, monitor output dir size to check whether it is finished (HACK FOR NOW)
-    # should use proper async monitor
-    print('Downloading ' + nm + ' (' + str(len(loc_urls)) + ' files)', '...')
+    # download order
+    print('Downloading ' + nm + ' (' + str(len(download_urls)) + ' files)', '...')
     callback = api.write_to_file(directory=dirname, overwrite=True)
     client.download_order(id, callback=callback)
 
+    # monitor output dir size to check whether it is finished (HACK FOR NOW), should use proper async monitor
+    # although client will continue downloading files to each folder one by one by itself
     dirsize = 1
     dirsize_old = 0
     while dirsize_old < dirsize:
         dirsize_old = dirsize
-        time.sleep(10)
+        time.sleep(30)
         dirsize = sum(os.path.getsize(os.path.join(dirname, f)) for f in os.listdir(dirname) if not f.endswith('.tmp'))
 
-    print('Finished downloading ' + nm)
-
-
-
-
-
-
+    print('Finished downloading', nm)
